c2273bc887b5d26a196a8324a4ee49bdcfc1490c
leil4zamani1383-cloud <leil4zamani1383@gmail.com>
2025-12-14 19:00:01 +0330

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './+public/+pages/login-page/login-page';
+import { PrivateTemplat } from './+private/private-templat/private-templat';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginPage},
+    {path:'private',component:PrivateTemplat,children:[
+
+    ]},
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'**',redirectTo:'login'}
+];

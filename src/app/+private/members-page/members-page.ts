4af93f7059300ad5ac7a5d6657691f12248b2a4a
leil4zamani1383-cloud <leil4zamani1383@gmail.com>
2025-12-22 21:57:57 +0330

    payanmember

--- src/app/+private/members-page/members-page.ts
+++ src/app/+private/members-page/members-page.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from './members-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-members-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './members-page.html',
   styleUrl: './members-page.scss',
 })
-export class MembersPage {
-
+export class MembersPage implements OnInit {
+  save() {
+    this.memberService.add(this.item);
+    this.dataRefresh();
+    this.state = 'list';
+  }
+  ngOnInit(): void {
+    this.dataRefresh();
+  }
+  data: MemberItem[] = [];
+  item: MemberItem = {
+    id: 0,
+    Firstname: '',
+    lastname: '',
+    Phonenumber: 0,
+    Nationalcode: 0,
+  };
+  memberService = inject(MembersService);
+  state: string = 'list';
+  dataRefresh() {
+    this.data = this.memberService.list();
+  }
+  add() {
+    this.state ='add';
+  }
+  cancel() {
+    this.state ='list';
+  }
+}
+export interface MemberItem {
+  id: number;
+  Firstname: string;
+  lastname: string;
+  Phonenumber: number;
+  Nationalcode: number;
 }
---
+++ src/app/+private/members-page/members-service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { MemberItem } from './members-page';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MembersService {
+  private data: MemberItem[] = [
+    {
+      id: 1,
+      Firstname: 'لیلا',
+      lastname: 'زمانی',
+      Phonenumber: 9185456452,
+      Nationalcode: 4020538024,
+    },
+    {
+      id: 2,
+      Firstname: 'فرشته',
+      lastname: 'قصبستانی',
+      Phonenumber: 9188965404,
+      Nationalcode: 4020538025,
+    },
+    {
+      id: 3,
+      Firstname: 'زینب',
+      lastname: 'رمضانی',
+      Phonenumber: 9181111111,
+      Nationalcode: 4020536026,
+    },
+    {
+      id: 4,
+      Firstname: 'نازنین',
+      lastname: 'مسلمی',
+      Phonenumber: 9182222222,
+      Nationalcode: 4020535026,
+    },
+    {
+      id: 5,
+      Firstname: 'پریا',
+      lastname: 'مومیموند',
+      Phonenumber: 9180000000,
+      Nationalcode: 4020534026,
+    },
+  ];
+  list() {
+    return [...this.data];
+  }
+  add(item: MemberItem) {
+    this.data.push(item);
+  }
+}
